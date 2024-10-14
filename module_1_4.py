a36859771413035aeeefe56bc78cbaae576dc8a6
seelk <sedova08111984@gmail.com>
2024-10-14 13:16:36 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+first_name = 'Курс: Python,'
+tasks_count = '12,'
+hours_count = '1.5,'
+hour_count = '0.125'
+print(first_name,'всего задач:',tasks_count,'затрачено часов:',hours_count,'среднее время выполнения',hour_count,'часа.')
